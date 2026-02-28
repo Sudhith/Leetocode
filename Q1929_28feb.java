aaf2a99788c1101c3c9a91946b7c7c7e9a9f0e9b
Sudith <saisudhithdevadatta@gmail.com>
2026-02-28 23:37:48 +0530

    Fixed small errors

--- Q1929_28feb.java
+++ Q1929_28feb.java
@@ -2,12 +2,12 @@ public class Q1929_28feb
 {
     public int[] getConcatenation(int[] nums) 
     {
-        int n = nums.length;
-        int[] ans = new int[2 * n];
-        for(int i = 0; i < n; i++)
+        int n1 = nums.length;
+        int[] ans = new int[2 * n1];
+        for(int i = 0; i < n1; i++)
         {
             ans[i] = nums[i];
-            ans[i + n] = nums[i];  
+            ans[i + n1] = nums[i];  
         }
 
         return ans;
