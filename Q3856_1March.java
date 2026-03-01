bc3ba59c5c9de6c773725eb95971bbce45bd6165
Sudith <saisudhithdevadatta@gmail.com>
2026-03-01 23:09:40 +0530

    fixed errors

--- Q3856_1March.java
+++ Q3856_1March.java
@@ -2,16 +2,16 @@ public class Q3856_1March
 {
     public String trimTrailingVowels(String s) 
     {
-        int i = s.length() - 1;
+        int j = s.length() - 1;
 
-        while(i >= 0)
+        while(j >= 0)
         {
-            char c = s.charAt(i);
+            char c = s.charAt(j);
 
             if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
                c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
             {
-                i--;
+                j--;
             }
             else
             {
@@ -19,6 +19,6 @@ public class Q3856_1March
             }
         }
 
-        return s.substring(0, i+1);
+        return s.substring(0, j+1);
     }
 }
