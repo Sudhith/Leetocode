5e200daf094e228b134fec87ddf10777c2de41b7
Sudith <saisudhithdevadatta@gmail.com>
2026-03-13 23:47:41 +0530

    Added Q45(MED)

---
+++ Q45_9March.java
@@ -0,0 +1,29 @@
+public class Q45_9March 
+{
+    class Solution 
+{
+    public int jump(int[] nums) 
+    {
+        int jumps = 0;
+        int end = 0;
+        int farthest = 0;
+
+        for(int i = 0; i < nums.length - 1; i++)
+        {
+            if(i + nums[i] > farthest)
+            {
+                farthest = i + nums[i];
+            }
+
+            if(i == end)
+            {
+                jumps++;
+                end = farthest;
+            }
+        }
+
+        return jumps;
+    }
+}
+    
+}
